9cad2434bd026e28824c9a6198bc13a52bb945b9
Sudhir <82999615+devSudhir@users.noreply.github.com>
2021-08-23 15:01:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { Home } from "./Component/Home";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Booking WebSite</h1>
+      <Home />
+    </div>
+  );
+}
---
+++ src/Component/Home.jsx
@@ -0,0 +1,9 @@
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+export const Home = () => {
+  return (
+    <Box component="span" m={1} style={{ border: "1px solid green" }}>
+      <Button />
+    </Box>
+  );
+};
